51c138551961a0ed31c557a475c6b621a8e914a1
florian <mrvdpflorian@gmail.com>
2022-02-03 15:33:56 +0100

    fin

--- src/Controller/ResultsController.php
+++ src/Controller/ResultsController.php
@@ -30,7 +30,10 @@ class ResultsController extends AbstractController
         $results = $response["results"];
         $total_pages = $response["total_pages"];
         $total_results = $response["total_results"];
+        dump($results);
+        //die();
         return $this->render('search/index.html.twig', [
+            "type" => $type,
             "search" => $search,
             "page" => $page,
             "results" => $results,
@@ -38,7 +41,23 @@ class ResultsController extends AbstractController
             "total_results" => $total_results
         ]);
     }
+
+
+    #[Route('/movies/{id}', name: 'movie')]
+    public function singleMovie($id, RequestStack $requestStack, HttpClientInterface $client)
+    {
+        $baseUrl = "https://api.themoviedb.org/3/";
+        $apiKey = "5ebe0843b2e373ffa159f5683b21b7de";
+        $lang = "en-US";
+        $url = $baseUrl."movie/$id"."?api_key=".$apiKey."&language=".$lang;
+        $response = $client->request(
+            'GET',
+            $url
+        );
+        $response = json_decode($response->getContent(), true);
+        dump($response);
+        return $this->render('search/single.html.twig', [
+            "results" => $response
+        ]);
+    }
 }
-//https://api.themoviedb.org/3/search/person?api_key=5ebe0843b2e373ffa159f5683b21b7de&language=en-US&page=1&include_adult=false&query=test
-//https://api.themoviedb.org/3/search/people?api_key=5ebe0843b2e373ffa159f5683b21b7de&language=en-US&page=1&include_adult=false&query=test
-//https://api.themoviedb.org/3/search/person?api_key=5ebe0843b2e373ffa159f5683b21b7de&language=en-US&page=1&query=test
